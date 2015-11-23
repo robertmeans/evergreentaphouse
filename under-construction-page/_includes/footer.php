61c22b9ac6db3e1824d6078478bcb56db6ba2737
Robert Means <robert@robertmeans.com>
2015-11-22 19:03:16 -0700

    phone number update

--- under-construction-page/_includes/footer.php
+++ under-construction-page/_includes/footer.php
@@ -6,7 +6,7 @@
 		<p><i class="fa fa-map-marker"></i> 2962 Evergreen Parkway</p>
 	</div>
 	<div id="right">
-		<p><i class="fa fa-phone-square"></i> <a class="tel" tabIndex="-1" href="tel:(303) 674-3323">303.674.3323</a></p>
+		<p><i class="fa fa-phone-square"></i> <a class="tel" tabIndex="-1" href="tel:(303) 674-3323">303.736.8419</a></p>
 	</div>
 	<div class="copyright"><p>&copy; 2015 Evergreen Tap House | <a href="http://www.evergreenwebdesign.com" target="_blank">Evergreen Web Design</a></p></div>
 </footer>
