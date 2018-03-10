3aa93822e1cbadf838b370ae8e7c8126e126b4c0
Evergreen Web Design <info@evergreenwebdesign.com>
2018-03-10 09:32:47 -0700

    updated redirects

---
+++ _redirect-to-evergreenbrewery/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="expires" content="0">
+	
+	<title>Evergreen Brewery</title>
+	<link rel="icon" type="image/ico" href="_images/favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
+
+</head>
+<body>
+<div id="redirect">
+  <p>By any name we're still the same.<br />
+  <a class="first" href="http://www.evergreenbrewery.com">Click here</a> to be redirected to our new address <a href="http://www.evergreenbrewery.com">EvergreenBrewery.com</a></p>
+</div>
+<script src="http://localhost:35729/livereload.js"></script>	
+</body>
+</html>
